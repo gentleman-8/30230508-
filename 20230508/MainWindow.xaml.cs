fa7a2aa22891363dd8b487e49f173aa7b36f6f17
gentleman-8 <roc.no2004@gmail.com>
2023-05-08 14:30:39 +0800

    9.增加%按鍵
    9.增加%按鍵

--- 20230508/MainWindow.xaml.cs
+++ 20230508/MainWindow.xaml.cs
@@ -32,6 +32,12 @@ namespace _20230508
                 txtNumber.Text = "";
             txtNumber.Text = txtNumber.Text + _number;
         }
+        private void Select_Operator(int _operator)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
+            txtNumber.Text = "0"; //重新將輸入文字框重新設定為0
+            operators = _operator; //選擇「加」號
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Add_Number("1"); // 只需要呼叫Add_Number函式，並且設定參數為數字1
@@ -62,7 +68,7 @@ namespace _20230508
 
         private void btnDivide_Click(object sender, RoutedEventArgs e)
         {
-
+            Select_Operator(3);
         }
 
         private void btnFour_Click(object sender, RoutedEventArgs e)
@@ -82,7 +88,7 @@ namespace _20230508
 
         private void btnPlus_Click(object sender, RoutedEventArgs e)
         {
-
+            Select_Operator(2);
         }
 
         private void btnTwo_Click(object sender, RoutedEventArgs e)
@@ -97,7 +103,7 @@ namespace _20230508
 
         private void btnMinus_Click(object sender, RoutedEventArgs e)
         {
-
+            Select_Operator(1);
         }
 
         private void btnDot_Click(object sender, RoutedEventArgs e)
@@ -141,11 +147,42 @@ namespace _20230508
             firstNumber = 0f;
             secondNumber = 0f;
             operators = -1;
-        } 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            float finalResults = 0f; //宣告最後計算結果變數
+            secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
+
+            //依照四則運算符號的選擇，進行加減乘除
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+            finalResults /= 100;
+            txtNumber.Text = string.Format("{0:p2}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
+
+            //重置所有全域變數
+
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-
+            Select_Operator(0);
         }
     }
 }
